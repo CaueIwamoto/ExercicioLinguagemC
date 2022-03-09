3741829c398ec30bf127fd59375738970756013e
Cauê Iwamoto Meira <92590617+CaueIwamoto@users.noreply.github.com>
2022-03-09 20:02:20 -0300

    Create ExdeC08,03,2022.c

---
+++ ExdeC08,03,2022.c
@@ -0,0 +1,44 @@
+//Exercícios laboratório algoritmo linguagem C - 08/03/2022
+
+//Exercício 1
+#include <stdio.h>
+#include <math.h>
+
+int main() {
+    float x1, y1;
+    float x2, y2;
+    
+    printf("Digite as coordenadas do 1o. ponto:\n");
+    scanf("%f %f", &x1, &y1);
+    
+    printf("Digite as coordenadas do 2o. ponto:\n");
+    scanf("%f %f", &x2, &y2);
+    
+    printf("Distancia entre os dois pontos: ");
+    
+    printf("%f",(sqrt(pow(x2-x1, 2) + pow (y2-y1, 2))));
+    
+    return 0;
+}
+
+//Exercício 2
+
+//Exercício 3
+
+//Exercício 4
+
+//Exercício 5
+
+//Exercício 6
+
+//Exercício 7
+
+//Exercício 8
+
+//Exercício 9
+
+//Exercício 10
+
+//Exercício 11
+
+//Exercício 12
