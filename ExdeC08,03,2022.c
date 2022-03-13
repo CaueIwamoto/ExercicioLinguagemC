9f49a68cbeee2cb599ae6f057c49e8ed1647a2b3
Cauê Iwamoto Meira <92590617+CaueIwamoto@users.noreply.github.com>
2022-03-13 12:27:42 -0300

    Update ExdeC08,03,2022.c

--- ExdeC08,03,2022.c
+++ ExdeC08,03,2022.c
@@ -28,6 +28,23 @@ int main() {
 //Exercício 4
 
 //Exercício 5
+#include <stdlib.h>
+#include <stdio.h>
+#include <math.h>
+
+
+void arccos(float entrada){
+    printf("\nO arccos de %f eh %.2f graus.", entrada, (acos(entrada)*180)/3.14159265358979323846);
+
+}
+
+int main(){
+    
+    float entrada;
+    printf("Digite o valor do arccos:");
+    scanf("%f", &entrada);
+    arccos(entrada);
+}
 
 //Exercício 6
 
@@ -36,9 +53,49 @@ int main() {
 //Exercício 8
 
 //Exercício 9
+#include <stdlib.h>
+#include <stdio.h>
+#include <math.h>
+
+int main(){
+    float entrada2, soma;
+    int entrada1;
+    soma = 0;
+    
+    scanf("%d", &entrada1);
+
+    for (int i = entrada1; i > 0; i--){
+        scanf("%f", &entrada2);
+        soma = entrada2+soma;
+    }
+    printf("Media = %f", soma/entrada1);
+
+}
 
 //Exercício 10
 
 //Exercício 11
+#include <stdio.h>
+#include <stdlib.h>
+#include <math.h>
+
+
+int main(){
+    int entrada, teste;
+    teste = 0;
+    printf("Digite um numero inteiro e positivo:");
+    scanf("%d", &entrada);
+
+    for(int i = 2; i <= entrada; i++){
+        if(entrada%i == 0){
+            teste++;
+        }
+    }
+    if(teste >= 2){
+        printf("\nComposto");
+    }else{
+    printf("\nPrimo");
+    }
+}
 
 //Exercício 12
