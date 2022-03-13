ef71293b3e513a4337fb787e140caf2428d4943a
Cauê Iwamoto Meira <92590617+CaueIwamoto@users.noreply.github.com>
2022-03-13 14:21:03 -0300

    Update ExdeC08,03,2022.c

--- ExdeC08,03,2022.c
+++ ExdeC08,03,2022.c
@@ -73,6 +73,38 @@ int main(){
 }
 
 //Exercício 10
+#include <stdio.h>
+#include <math.h>
+
+int main() {
+    int QTD_ELEMENTOS;
+    scanf("%d", &QTD_ELEMENTOS);
+    int vetor[QTD_ELEMENTOS];
+
+    for (int i = 0; i < QTD_ELEMENTOS; i++) {
+        //printf("Digite um número: ");
+        scanf("%d", &vetor[i]);
+    }
+    
+    int somatorio = 0;
+    for (int i = 0; i < QTD_ELEMENTOS; i++) {
+        somatorio += vetor[i];
+    }
+
+    float media = somatorio / (float) QTD_ELEMENTOS;
+
+    float variacoes = 0;
+    for (int i = 0; i < QTD_ELEMENTOS; i++) {
+        float v = vetor[i] - media;
+        variacoes += v * v;
+    }
+
+    float sigma = sqrt(variacoes / (QTD_ELEMENTOS-1));
+    printf("Desvio Padrao Amostral = %f", sigma);
+    printf("\nVariancia Amostral = %f", sigma*sigma);
+    
+return 0;
+}
 
 //Exercício 11
 #include <stdio.h>
