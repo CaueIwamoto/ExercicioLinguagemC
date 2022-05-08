f85a01b3af582d7130ff055561d84e3987ca4bc5
Cauê Iwamoto Meira <92590617+CaueIwamoto@users.noreply.github.com>
2022-05-08 19:57:32 -0300

    Update ExRecursão.c

--- ExRecursão.c
+++ ExRecursão.c
@@ -39,6 +39,75 @@ double Pot(int num,int exp){
 }
 
 //Exercício 2
+#include <stdlib.h>
+#include <stdio.h>
+#include <math.h>
+
+
+void repete (int[],int,int,int);
+
+int main(){
+    int n;
+    scanf("%d",&n);
+    int vetor[n];
+    int temp;
+    for(int i=0;i<n;i++){
+        scanf("%d",vetor+i);
+        //printf("%d",vetor[i]);
+    }
+    
+     for (int i = 0; i < n; i++) {     
+         for (int j = i+1; j < n; j++) {     
+          if(vetor[i] > vetor[j]) {    
+              temp = vetor[i];    
+              vetor[i] =vetor[j];    
+              vetor[j] = temp;    
+          }     
+  }
+     }
+//for(int i=0;i<n;i++){
+     //   printf("%d",vetor[i]);
+    //}
+    repete(vetor,0,n,1);
+}
+/*
+void repete (int v[],int pos,int total){
+    int auxiliar = v[pos];
+    int conta=0;
+if (pos>total-1){
+        return;}
+    
+    for(int i=pos;i<total;i++){
+        if (v[i]!=auxiliar){
+            break;
+        }
+        conta+=1;
+    }
+    
+    
+    printf("%d aparece %d vez(es)\n",v[pos],conta);
+   // printf("%d\n",pos);
+   return repete(v,pos+conta,total);
+    
+}*/
+void repete (int v[],int pos,int total, int conta){
+    
+if (pos>total-1){
+        return;}
+    
+    if (v[pos]!=v[pos+1]){
+printf("%d aparece %d vez(es)\n",v[pos],conta);
+   // printf("%d\n",pos);
+        return repete(v,pos+1,total,1);
+    }
+    
+    
+    
+   return repete(v,pos+1,total,conta + 1);
+    
+}
+
+//printf("%d aparece %d vez(es)\n", v[j-1], p);
 
 
 //Exercício 3
@@ -99,3 +168,30 @@ double f(int n){
 }
 
 //Exercício 5
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <math.h>
+
+int count = 0;
+int res = 1;
+int C(int n){
+    if (count == n || n == 1){
+        printf("%d ", n);
+        return n;
+    }else{
+        count++;
+        printf("%d ", n);
+        if (n%2==0){
+            n= n / 2;
+        }else if(n%2==1){
+            n= (3 *n) + 1;
+        }
+        return C(n);
+    }
+}
+int main(){
+    int n;
+    scanf("%d", &n);
+    C(n);
+}
